4919d7f0a88520fd3089519595c8a375368ed444
Yung Siang Liau <liauys@gmail.com>
2017-02-03 02:29:13 +0800

    Remove interactive mode

--- app/Main.hs
+++ app/Main.hs
@@ -4,25 +4,8 @@ import           Language.LOLCODE.Interpreter
 import           Language.LOLCODE.Parser
 
 import           Control.Monad.Trans
-import           System.Console.Haskeline
 import           System.Environment
 import           System.IO
-
-process :: String -> IO ()
-process line = do
-  let res = parseToplevel line
-  case res of
-    Left err -> print err
-    Right ex -> mapM_ print ex
-
-runInteractive :: IO ()
-runInteractive = runInputT defaultSettings loop
-  where
-  loop = do
-    minput <- getInputLine "ready> "
-    case minput of
-      Nothing -> outputStrLn "Goodbye."
-      Just input -> (liftIO $ process input) >> loop
 
 parseFile :: String -> IO ()
 parseFile filename = do
@@ -38,6 +21,5 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [] -> runInteractive
     [filename] -> parseFile filename
-    _ -> putStrLn "Usage: hcli [filename]"
+    _ -> putStrLn "Usage: hcli filename"
